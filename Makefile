8dbf0bc61db77459e0285d942d303bc57c771bde
navaneethkn <navaneethkn@gmail.com>
2014-04-01 21:54:48 +0530

    Rewrote Java process management in golang
    Previously, we have used shell scripts to manage the Java process. This
    is becoming unmaintainable and each platform needs it's own
    implemntation. So rewriting the process management in Golang so that
    same code can be used on all the platforms
    
    This implementation respects some environment variables to alter the
    classpath and JVM args etc. It reads the classpath, convert all of them
    into absolute path before starting the java process. Classpath will be
    specified as a comma separated list in the env variable
    
    This also allows switching Java versions so that tests can target
    different java versions

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ endif
 PROGRAM_NAME=twist2
 
 default: clean
+	go build
 	ant jar
 
 clean:
@@ -17,11 +18,10 @@ install:
 	install -m 644 libs/* $(prefix)/lib/$(PROGRAM_NAME)/java/libs
 	install -m 644 build/jar/* $(prefix)/lib/$(PROGRAM_NAME)/java
 	install -m 755 -d $(prefix)/bin
-	install -m 755 twist-java.sh $(prefix)/bin
-	install -m 755 twist-java-project-init.sh $(prefix)/bin
+	install -m 755 twist2-java $(prefix)/bin
 	install -m 755 -d $(prefix)/share/$(PROGRAM_NAME)/languages
 	install -m 644 java.json $(prefix)/share/$(PROGRAM_NAME)/languages
 	install -m 755 -d $(prefix)/share/$(PROGRAM_NAME)/skel/java
 	install -m 755 -d $(prefix)/share/$(PROGRAM_NAME)/skel/env
 	install -m 644 skel/StepImplementation.java $(prefix)/share/$(PROGRAM_NAME)/skel/java
-	install -m 644 skel/classpath.json $(prefix)/share/$(PROGRAM_NAME)/skel/env
+	install -m 644 skel/java.json $(prefix)/share/$(PROGRAM_NAME)/skel/env
---
+++ twist-java.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	newDirectoryPermissions   = 0755
+	newFilePermissions        = 0644
+	executable_path           = "twist2_java_executable"
+	additional_libs_env_name  = "twist2_java_additional_libs"
+	classpath_env_name        = "twist2_java_classpath"
+	jvm_args_env_name         = "twist2_java_jvm_args"
+	main_class_name           = "com.thoughtworks.twist2.TwistRuntime"
+	step_implementation_class = "StepImplementation.java"
+)
+
+func appendClasspath(source *string, classpath string) {
+	if len(*source) == 0 {
+		*source = classpath
+	} else {
+		*source = fmt.Sprintf("%s%c%s", *source, os.PathListSeparator, classpath)
+	}
+}
+
+var projectRoot = ""
+var start = flag.Bool("start", false, "Start the java runner")
+var initialize = flag.Bool("init", false, "Initialize the java runner")
+
+func dirExists(dirPath string) bool {
+	stat, err := os.Stat(dirPath)
+	if err == nil && stat.IsDir() {
+		return true
+	}
+
+	return false
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+func getInstallationPath() string {
+	possibleInstallationPaths := []string{"/usr/local/lib/twist2/java", "/usr/lib/twist2/java"}
+	for _, p := range possibleInstallationPaths {
+		if dirExists(p) {
+			return p
+		}
+	}
+
+	panic(errors.New("Can't find installation files"))
+}
+
+func getInstallationSharePath() string {
+	possibleInstallationPaths := []string{"/usr/local/share/twist2", "/usr/share/twist2"}
+	for _, p := range possibleInstallationPaths {
+		if dirExists(p) {
+			return p
+		}
+	}
+
+	panic(errors.New("Can't find installation files"))
+}
+
+func getProjectRoot() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	return pwd
+}
+
+func getIntelliJClasspath() string {
+	intellijOutDir := path.Join(getProjectRoot(), "out", "production")
+	if !dirExists(intellijOutDir) {
+		return ""
+	}
+
+	cp := ""
+	walker := func(path string, info os.FileInfo, err error) error {
+		if path == intellijOutDir {
+			return nil
+		}
+		if info.IsDir() {
+			appendClasspath(&cp, path)
+			// we need only top-level directories. Don't walk nested
+			return filepath.SkipDir
+		}
+		return nil
+	}
+	filepath.Walk(intellijOutDir, walker)
+	return cp
+}
+
+func getEclipseClasspath() string {
+	eclipseOutDir := path.Join(getProjectRoot(), "bin")
+	if !dirExists(eclipseOutDir) {
+		return ""
+	}
+
+	return eclipseOutDir
+}
+
+// User set classpath & additional libs will be comma separated
+// it could be relative path, but JVM needs full path to be specified
+// so this function splits the path, convert them to absolute path forms a classpath
+func getClassPathForVariable(envVariableName string) string {
+	value := os.Getenv(envVariableName)
+	cp := ""
+	if len(value) > 0 {
+		paths := strings.Split(value, ",")
+		for _, p := range paths {
+			abs, err := filepath.Abs(p)
+			if err == nil {
+				appendClasspath(&cp, abs)
+			} else {
+				appendClasspath(&cp, p)
+			}
+		}
+	}
+	return cp
+}
+
+func copyFile(src, dest string) error {
+	if !fileExists(src) {
+		return errors.New(fmt.Sprintf("%s doesn't exist", src))
+	}
+
+	b, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(dest, b, newFilePermissions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type initializerFunc func()
+
+func showMessage(action, filename string) {
+	fmt.Printf(" %s  %s\n", action, filename)
+}
+
+func createSrcDirectory() {
+	showMessage("create", "src/")
+	if !dirExists("src") {
+		err := os.Mkdir("src", 0755)
+		if err != nil {
+			fmt.Printf("Failed to make directory. %s\n", err.Error())
+		}
+	} else {
+		showMessage("skip", "src/")
+	}
+}
+
+func createStepImplementationClass() {
+	destFile := path.Join("src", step_implementation_class)
+	showMessage("create", destFile)
+	if fileExists(destFile) {
+		showMessage("skip", destFile)
+	} else {
+		srcFile := path.Join(getInstallationSharePath(), "skel", "java", step_implementation_class)
+		err := copyFile(srcFile, destFile)
+		if err != nil {
+			showMessage("error", fmt.Sprintf("Failed to copy %s. %s", srcFile, err.Error()))
+		}
+	}
+}
+
+func createJavaJSON() {
+	destFile := path.Join("env", "default", "java.json")
+	showMessage("create", destFile)
+	if fileExists(destFile) {
+		showMessage("skip", destFile)
+	} else {
+		srcFile := path.Join(getInstallationSharePath(), "skel", "env", "java.json")
+		err := copyFile(srcFile, destFile)
+		if err != nil {
+			showMessage("error", fmt.Sprintf("Failed to copy %s. %s", srcFile, err.Error()))
+		}
+	}
+}
+
+func printUsage() {
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Parse()
+	if *start {
+		cp := ""
+		appendClasspath(&cp, path.Join(getInstallationPath(), "*"))
+		appendClasspath(&cp, path.Join(getInstallationPath(), "libs", "*"))
+
+		// If user has specified classpath, that will be taken. If not search for IntelliJ and Eclipse out directories before giving up
+		userSpecifiedClasspath := getClassPathForVariable(classpath_env_name)
+		if userSpecifiedClasspath != "" {
+			appendClasspath(&cp, userSpecifiedClasspath)
+		} else {
+			cpfound := false
+			icp := getIntelliJClasspath()
+			if icp != "" {
+				appendClasspath(&cp, icp)
+				cpfound = true
+			} else {
+				ecp := getEclipseClasspath()
+				if ecp != "" {
+					appendClasspath(&cp, ecp)
+					cpfound = true
+				}
+			}
+
+			if !cpfound {
+				fmt.Println("Failed to detect project build path")
+				fmt.Printf("Set \"%s\" variable to the build directory\n", classpath_env_name)
+				fmt.Printf("Set \"%s\" variable to specify dependent libraries\n", additional_libs_env_name)
+				fmt.Println("These variables can be set as environment variables or specify it in env/default/java.json file")
+				os.Exit(2)
+			}
+		}
+
+		javaPath := os.Getenv(executable_path)
+		if javaPath == "" {
+			javaPath = "java"
+		}
+		args := []string{"-classpath", cp}
+		if os.Getenv(jvm_args_env_name) != "" {
+			args = append(args, os.Getenv(jvm_args_env_name))
+		}
+		args = append(args, main_class_name)
+		cmd := exec.Command(javaPath, args...)
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to start Java. %s\n", err.Error())
+			os.Exit(1)
+		}
+	} else if *initialize {
+		funcs := []initializerFunc{createSrcDirectory, createStepImplementationClass, createJavaJSON}
+		for _, f := range funcs {
+			f()
+		}
+	} else {
+		printUsage()
+	}
+}
